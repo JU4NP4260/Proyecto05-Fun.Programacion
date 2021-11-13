c2be466d45d00e7f0fdfdf9a7b87f86222a5285a
Juan Camilo De Hoyos <jdehoyos555@gmail.com>
2021-11-13 17:21:20 -0500

    creación del objeto "Lanzamiento"

---
+++ ColonizacionMarte/ColonizacionMarte/Lanzamiento.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColonizacionMarte
+{
+    class Lanzamiento
+    {
+        public string planicie { get; set; }
+        public float carga { get; set; }
+
+        public Lanzamiento()
+        {
+            planicie = "";
+            carga = 0;
+        }
+    }
+}
